9073275295f8d7530e29313a604da0c09a7effb4
Kamil Dec <67755708+deckamil@users.noreply.github.com>
2022-12-07 17:50:00 +0100

    Included functionality that finds internal interface details, i.e. local varaibales

--- MCG/MCG_CC/mcg_cc_file_reader.py
+++ MCG/MCG_CC/mcg_cc_file_reader.py
@@ -37,24 +37,10 @@ from mcg_cc_connection import Connection
 # Description:
 # This class allows to read model module content from .exml files.
 class FileReader(object):
-    # This parameter defines index of target element marker from list of target elements returned
-    # by find_target_element_name() method
-    TARGET_ELEMENT_FOUND_INDEX = 0
 
-    # This parameter defines index of target element name from list of target elements returned
-    # by find_target_element_name() method
-    TARGET_ELEMENT_NAME_INDEX = 1
-
-    # This parameter defines index of interface element name
+    # Indexes of interface element list
     INTERFACE_ELEMENT_NAME_INDEX = 0
-
-    # This parameter defines index of interface element type
     INTERFACE_ELEMENT_TYPE_INDEX = 1
-
-    # This list defines all valid interface types
-    interface_type_list = ["INT8", "INT16", "INT32", "INT64",
-                           "UINT8", "UINT16", "UINT32", "UINT64",
-                           "FLOAT32", "FLOAT64"]
 
     # Indexes of reader list
     CONNECTION_LIST_INDEX = 0
@@ -75,98 +61,7 @@ class FileReader(object):
         self.interaction_list = []
         self.input_interface_list = []
         self.output_interface_list = []
-        self.local_data_list = []
-
-    # Description:
-    # This method checks if reference contains valid interface element type
-    @staticmethod
-    def check_if_interface_element_type(ref_interface_element_type, interface_element):
-        # interface element type marker shows whether valid interface element type was found or not within reference
-        interface_element_type_found = False
-
-        # if signal is interface element under check
-        if interface_element == "signal":
-
-            # for all allowed signal types
-            for interface_signal_type in FileReader.interface_signal_type_list:
-                # if interface signal type is the same as in reference
-                if interface_signal_type == ref_interface_element_type:
-                    # change interface element type marker
-                    interface_element_type_found = True
-                    # exit loop
-                    break
-
-        # else if structure is interface element under check
-        else:
-
-            # for all allowed structure types
-            for interface_structure_type in FileReader.interface_structure_type_list:
-                # if interface structure type is the same as in reference
-                if interface_structure_type == ref_interface_element_type:
-                    # change interface element type marker
-                    interface_element_type_found = True
-                    # exit loop
-                    break
-
-        # return interface element type marker
-        return interface_element_type_found
-
-    # Description:
-    # This method looks for <name> element, basing on target element type and its uid, within
-    # content of .exml file, an example of .exml file line:
-    # <ID name="input3" mc="Standard.Attribute" uid="338540aa-439c-4dc7-8414-a275ba3c08e1"/>
-    def find_target_element_name(self, target_element_uid, target_element_type):
-
-        # target element marker shows whether target element was found or not
-        target_element_found = False
-        # target element name
-        target_element_name = ""
-
-        # search for uid in file content
-        for i in range(0, len(self.activity_file)):
-
-            # if uid within the line
-            if ("<OBJECT>" in self.activity_file[i]) and \
-                    ("<ID name=" in self.activity_file[i + 1]) and \
-                    (target_element_uid in self.activity_file[i + 1]):
-
-                # search for target element definition
-                for j in range(i + 1, len(self.activity_file)):
-
-                    # if given line contains definition of target element
-                    if ("<ID name=" in self.activity_file[j]) and \
-                            (target_element_type in self.activity_file[j]):
-                        # get line
-                        line = self.activity_file[j]
-                        # get line number
-                        line_number = j + 1
-                        # get target element name
-                        target_element_name = FileReader.get_name(line, line_number)
-                        # change target element marker
-                        target_element_found = True
-                        # exit "for j in range" loop
-                        break
-
-                    # if line contains </OBJECT> that means end of object definition
-                    if "</OBJECT>" in self.activity_file[j]:
-                        # exit "for j in range" loop
-                        break
-
-                # exit "for i in range" loop
-                break
-
-        # if target element has not been found
-        if not target_element_found:
-            # set target element name
-            target_element_name = "TARGET_ELEMENT_NAME_NOT_FOUND"
-
-        # append collected data to target element list
-        target_element_list = []
-        target_element_list.insert(FileReader.TARGET_ELEMENT_FOUND_INDEX, target_element_found)
-        target_element_list.insert(FileReader.TARGET_ELEMENT_NAME_INDEX, target_element_name)
-
-        # return target element list
-        return target_element_list
+        self.local_interface_list = []
 
     # Description:
     # This method looks for interface details of module operation.
@@ -175,7 +70,8 @@ class FileReader(object):
         # record info
         Logger.save_in_log_file("Reader", "Looking for module interface details in .exml file", False)
 
-        # search for interface details of operation in module file
+        # search for external interface details of operation in module file
+        # i.e. operation input and output parameters
         for i in range(0, len(self.module_file)):
 
             # parameter details
@@ -183,7 +79,7 @@ class FileReader(object):
             parameter_direction = "UNKNOWN_DIRECTION"
             parameter_type = "UNKNOWN_TYPE"
 
-            # if parameter name if found
+            # if parameter section if found
             if "<ID name=" in self.module_file[i] and "mc=\"Standard.Parameter\"" in self.module_file[i]:
                 # get parameter name
                 parameter_name = Supporter.get_name(self.module_file[i])
@@ -224,12 +120,52 @@ class FileReader(object):
                         # exit 'for j in range' loop
                         break
 
+        # search for internal interface details of operation in activity file
+        # i.e. operation local variables
+        for i in range(0, len(self.activity_file)):
+
+            # local details
+            local_name = "UNKNOWN_NAME"
+            local_type = "UNKNOWN_TYPE"
+
+            # if local section if found
+            if "<OBJECT>" in self.activity_file[i] and \
+                    "<ID name=" in self.activity_file[i+1] and \
+                    "mc=\"Standard.InstanceNode\"" in self.activity_file[i+1]:
+                # get local name
+                local_name = Supporter.get_name(self.activity_file[i+1])
+
+                # search for local direction and type
+                for j in range(i, len(self.activity_file)):
+
+                    # if local type is found
+                    if "<ID name=" in self.activity_file[j] and "mc=\"Standard.DataType\"" in self.activity_file[j]:
+                        # get local type
+                        local_type = Supporter.get_name(self.activity_file[j])
+
+                        # interface element
+                        interface_element = []
+
+                        # append local name to interface element
+                        interface_element.insert(FileReader.INTERFACE_ELEMENT_NAME_INDEX, local_name)
+                        # append local type to interface element
+                        interface_element.insert(FileReader.INTERFACE_ELEMENT_TYPE_INDEX, local_type)
+                        # append interface element to interface list
+                        if interface_element not in self.local_interface_list:
+                            self.local_interface_list.append(interface_element)
+
+                        # exit 'for j in range' loop
+                        break
+
         # record info
         for input_interface in self.input_interface_list:
             Logger.save_in_log_file("Reader", "Have found input interface " + str(input_interface) + " element",
                                     False)
         for output_interface in self.output_interface_list:
             Logger.save_in_log_file("Reader", "Have found output interface " + str(output_interface) + " element",
+                                    False)
+        for local_interface in self.local_interface_list:
+            Logger.save_in_log_file("Reader", "Have found local interface " + str(local_interface) + " element",
                                     False)
 
     # Description:
@@ -511,9 +447,6 @@ class FileReader(object):
 
         # search for interaction targets
         self.read_interaction_targets()
-
-        # check module correctness
-        # self.check_correctness()
 
         # append collected data to module reader list
         # component_reader_list = []
